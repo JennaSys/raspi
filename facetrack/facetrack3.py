ff5a532914bbb828d09b9875c3e1732e4131bf5c
John Sheehan <jennasyseng@mail.com>
2018-08-19 16:41:49 -0700

    formatting update

--- facetrack/facetrack3.py
+++ facetrack/facetrack3.py
@@ -222,7 +222,8 @@ try:
                 aframe = capture.grab()  # again we grab some frames, things may have gotten stale-
                 aframe = capture.grab()  # since the frontalface search above
                 aframe = capture.read()[1]
-                pfacer = profileface.detectMultiScale(aframe, 1.3, 4, (cv2.cv.CV_HAAR_DO_CANNY_PRUNING + cv2.cv.CV_HAAR_FIND_BIGGEST_OBJECT + cv2.cv.CV_HAAR_DO_ROUGH_SEARCH), (80, 80))
+                pfacer = profileface.detectMultiScale(aframe, 1.3, 4, (
+                        cv2.cv.CV_HAAR_DO_CANNY_PRUNING + cv2.cv.CV_HAAR_FIND_BIGGEST_OBJECT + cv2.cv.CV_HAAR_DO_ROUGH_SEARCH), (80, 80))
                 # pfacer = profileface.detectMultiScale(aframe, 1.1, 2, 0,(50,60))
                 if pfacer != ():  # if we found a profile face...
                     lastface = 2
@@ -239,8 +240,7 @@ try:
                 aframe = capture.read()[1]
                 cv2.flip(aframe, 1, aframe)  # flip the image
                 pfacel = profileface.detectMultiScale(aframe, 1.3, 4, (
-                            cv2.cv.CV_HAAR_DO_CANNY_PRUNING + cv2.cv.CV_HAAR_FIND_BIGGEST_OBJECT + cv2.cv.CV_HAAR_DO_ROUGH_SEARCH),
-                                                      (80, 80))
+                            cv2.cv.CV_HAAR_DO_CANNY_PRUNING + cv2.cv.CV_HAAR_FIND_BIGGEST_OBJECT + cv2.cv.CV_HAAR_DO_ROUGH_SEARCH), (80, 80))
                 # pfacel = profileface.detectMultiScale(aframe, 1.1, 2, 0, (50,60))
                 if pfacel != ():
                     lastface = 3
